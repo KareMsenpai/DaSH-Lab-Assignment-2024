91969389a2c389275024b02cc92bcf197eeb8144
KareMsenpai <164791025+KareMsenpai@users.noreply.github.com>
2024-08-28 01:19:30 +0530

    Add files via upload

---
+++ API_calls.py
@@ -0,0 +1,16 @@
+import google.generativeai as genai 
+import json
+import requests
+prompt = input("what is the prompt ")
+genai.configure(api_key = "AIzaSyAsxpuioHx9n-ToX9_Sp8ixuzpfMVd1xAU")
+model = genai.GenerativeModel("gemini-1.5-flash")
+response = model.generate_content(prompt)
+responses = []
+responses.append({
+    "prompt":f"{prompt}",
+    "response":response.text
+})
+with open("output.json","w") as file:
+    json.dump(responses,file)
+
+
