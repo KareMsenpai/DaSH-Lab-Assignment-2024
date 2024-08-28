478d12dfa3f74230a6edfed184dae6b68075b747
KareMsenpai <164791025+KareMsenpai@users.noreply.github.com>
2024-08-29 02:15:21 +0530

    API_calls.py

--- API_calls.py
+++ API_calls.py
@@ -1,4 +1,5 @@
 import google.generativeai as genai 
+import time
 import json
 import requests
 prompt = input("what is the prompt ")
@@ -6,10 +7,12 @@ genai.configure(api_key = "AIzaSyAsxpuioHx9n-ToX9_Sp8ixuzpfMVd1xAU")
 model = genai.GenerativeModel("gemini-1.5-flash")
 response = model.generate_content(prompt)
 responses = []
+time = time.time()
 responses.append({
     "prompt":f"{prompt}",
     "response":response.text,
-    "source":"gemini"
+    "source":"gemini",
+    "time":f"{time}"
 })
 with open("output.json","w") as file:
     json.dump(responses,file)
